377ae6fb030d258ce9690c59f6e0d40452b452eb
Sleman Kassis <slemankassis@hotmail.com>
2019-10-27 21:23:20 -0300

    Add constants file. Exceptions eslint. Add removeDuplicatesArray util.

---
+++ src/pages/Product/Constants.js
@@ -0,0 +1,10 @@
+module.exports = {
+  QTY_CHARS_FOR_BAND_SIZES: 2,
+  NAME_COLOR_VARIANTS: 'option1',
+  NAME_SIZE_VARIANTS: 'option2',
+  IMAGES: {
+    THUMBNAIL: 'src100',
+    ORIGINAL_MEDIUM: 'src600',
+    ORIGINAL_LARGE: 'src1000',
+  },
+};
