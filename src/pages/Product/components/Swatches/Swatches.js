4816997096d324438a7e8b5a3af5751544263abc
Sleman Kassis <slemankassis@hotmail.com>
2019-10-27 21:24:48 -0300

    Use of json path for search in json.

--- src/pages/Product/components/Swatches/Swatches.js
+++ src/pages/Product/components/Swatches/Swatches.js
@@ -3,8 +3,6 @@ import RoundSelector from '../../../../thirdy-part-components/RoundPicker';
 
 class Swatches extends React.Component {
   render() {
-    console.log(this.props);
-
     return (
       <RoundSelector />
     );
