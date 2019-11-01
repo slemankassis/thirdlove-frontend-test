2222278e2c8c30796112a626134301ae126bb39f
Sleman Kassis <slemankassis@hotmail.com>
2019-11-01 14:51:39 -0300

    Product skeleton.

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from '../../thirdy-part-components/Button';
+import Label from '../../thirdy-part-components/Label';
 import {
   THUMBNAIL,
   ORIGINAL_LARGE,
@@ -6,11 +8,12 @@ import {
   SIZE_VARIANTS,
   QTY_CHARS_FOR_BAND_SIZES,
   ID,
+  TITLE,
   PRICE,
   STOCK,
 } from './Constants';
 import Carousel from './components/Carousel';
-import ColorVariantsContainer from './components/ColorVariantsContainer';
+import VariantsContainer from './components/VariantsContainer';
 import Description from './components/Description';
 import './Product.scss';
 
@@ -27,6 +30,7 @@ const transformVariants = (variants) => (
   variants.reduce((transformedVariants, variant) => (variant[STOCK] >= 10
     ? transformedVariants.concat({
       id: variant[ID],
+      title: variant[TITLE],
       price: variant[PRICE],
       stock: variant[STOCK],
       color: variant[COLOR_VARIANTS],
@@ -38,12 +42,77 @@ const transformVariants = (variants) => (
   ), [])
 );
 
-const Product = ({ product: { images, variants, body_html: bodyHtml } }) => (
-  <React.Fragment>
-    <Carousel images={transformImages(images)} />
-    <ColorVariantsContainer variants={transformVariants(variants)} />
-    <Description contentHtml={bodyHtml} className="product-description" />
-  </React.Fragment>
-);
+class Product extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+    this.onchangeVariant = this.onchangeVariant.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.addToCart = this.addToCart.bind(this);
+
+    console.log(this.props.product.variants[0]);
+
+    this.state = {
+      selectedVariant: this.props.product.variants[0].id,
+    };
+  }
+
+  onchangeVariant(value) {
+    this.state(() => ({
+      selectedVariant: value,
+    }));
+  }
+
+  handleSubmit(title, productId, { band, cup, id: variantId }) {
+    alert(`Added a ${title} - ${band}${cup} to the cart`);
+    this.addToCart(productId, variantId);
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  addToCart(productId, varianId) { // eslint-disable-line class-methods-use-this
+    // TODO: Call service with productId and variantId and remove eslint exceptions
+  }
+
+  render() {
+    const {
+      product: {
+        id,
+        title,
+        price,
+        stock,
+        images,
+        variants,
+        body_html: bodyHtml,
+      },
+    } = this.props;
+    const { selectedVariant } = this.state;
+    return (
+      <React.Fragment>
+        <Carousel images={transformImages(images)} />
+        {/* TODO: Use form with input dropdown and radios */}
+        {/* <form onSubmit={() => this.handleSubmit(title, id, variants[selectedVariant])}> */}
+        <Label className="product-title" text={title} />
+        {variants[selectedVariant] && (
+          <Label className="product-color" text={`COLOR: ${variants[selectedVariant].color}`} />
+        )}
+        <Label className="product-price" text={price} />
+        <VariantsContainer
+          onChange={this.onchangeProduct}
+          variants={transformVariants(variants)}
+          selectedVariant={selectedVariant}
+        />
+        <Label className="product-stock" text={`STOCK: ${stock}`} />
+        <input type="submit" value="Submit" />
+        {/* </form> */}
+        <Button
+          type="action"
+          text="Add to Bag"
+          onClick={() => this.handleClick(title, id, variants[selectedVariant])}
+        />
+        <Description className="product-description" contentHtml={bodyHtml} />
+      </React.Fragment>
+    );
+  }
+}
 
 export default Product;
