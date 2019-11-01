d4abb0a747a14e7ee43cdbce367f5bca40a7138c
Sleman Kassis <slemankassis@hotmail.com>
2019-11-01 14:21:13 -0300

    Add ErrorBoundary component only.

---
+++ src/thirdy-part-components/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div className="error-boundary">{this.props.errorMessage}</div>;
+    }
+    return this.props.children;
+  }
+}
+
+// ErrorBoundary.propTypes = {
+//   errorMessage: PropTypes.string,
+//   children: PropTypes.oneOfType([
+//     PropTypes.element,
+//     PropTypes.arrayOf(PropTypes.element),
+//   ]).isRequired,
+// };
+
+// ErrorBoundary.defaultProps = {
+//   errorMessage: 'There was an error loading this component.',
+// };
+
+export default ErrorBoundary;
