7aa169ebcde04d53f59e363b8eb73e5cd9ab45ce
Sleman Kassis <slemankassis@hotmail.com>
2019-11-02 14:33:41 -0300

    Fix Error boundary component test.

--- src/thirdy-part-components/ErrorBoundary/ErrorBoundary.spec.js
+++ src/thirdy-part-components/ErrorBoundary/ErrorBoundary.spec.js
@@ -4,12 +4,7 @@ import ErrorBoundary from './ErrorBoundary';
 describe('ErrorBoundary snaps', () => {
   it('renders snapshot with basicProps', () => {
     const basicProps = {
-      selected: {
-        value: '__VALUE__',
-        label: '__LABEL__',
-      },
-      options: ['__OPTION_1__', '__OPTION_2__'],
-      onChange: jest.fn(),
+      children: React.createElement('div'),
     };
 
     const component = shallow(<ErrorBoundary {...basicProps} />);
@@ -18,12 +13,6 @@ describe('ErrorBoundary snaps', () => {
 
   it('renders snapshot with fullProps', () => {
     const fullProps = {
-      selected: {
-        value: '__VALUE__',
-        label: '__LABEL__',
-      },
-      options: ['__OPTION_1__', '__OPTION_2__'],
-      onChange: jest.fn(),
       errorMessage: '__ERROR_MESSAGE__',
       children: React.createElement('div'),
     };
