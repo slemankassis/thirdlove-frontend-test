51e1e29c75d533a4a3fe5f793889b12ec9908c81
Sleman Kassis <slemankassis@hotmail.com>
2019-11-03 23:42:34 -0300

    Updates

---
+++ precache-manifest.52ef06be2931842cb91aec89d4f8c820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a14278f69a645fae978f4f121e73129",
+    "url": "/thirdlove-frontend-test/index.html"
+  },
+  {
+    "revision": "303a523fa8f37c903c5c",
+    "url": "/thirdlove-frontend-test/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "ce61ffe0f4434a396b58",
+    "url": "/thirdlove-frontend-test/static/css/main.06d45eb2.chunk.css"
+  },
+  {
+    "revision": "303a523fa8f37c903c5c",
+    "url": "/thirdlove-frontend-test/static/js/2.1239f470.chunk.js"
+  },
+  {
+    "revision": "ce61ffe0f4434a396b58",
+    "url": "/thirdlove-frontend-test/static/js/main.840ce26d.chunk.js"
+  },
+  {
+    "revision": "6be405bc801d86b501b8",
+    "url": "/thirdlove-frontend-test/static/js/runtime-main.a403e70d.js"
+  }
+]);
